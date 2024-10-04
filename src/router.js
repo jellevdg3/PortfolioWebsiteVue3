ec93674f46b077c37e480b3a89d70062e6f3cdb8
Jelle van der Gulik <jellevdg3@gmail.com>
2024-10-04 17:06:18 +0200

    Update router

--- src/router.js
+++ src/router.js
@@ -1,10 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Projects from './views/Projects.vue'
-import About from './views/About.vue'
 
 const routes = [
-	{ path: '/', name: 'Home', component: About },
-	{ path: '/projects', name: 'Projects', component: Projects },
 ]
 
 const router = createRouter({
